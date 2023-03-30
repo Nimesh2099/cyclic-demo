e398bf9cf78c2db53defe31ee2897e1fe165fd30
Nimesh <chaudharynimesh3456@gmail.com>
2023-03-30 09:34:22 -0400

    Solve express decleration error

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import  express  from "express"
 
 var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
 var app = express();
 
  // setup a 'route' to listen on the default url path
