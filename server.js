1801c9a6e9ff8838ab8573f04c8c2de95363e37b
Nimesh <chaudharynimesh3456@gmail.com>
2023-03-30 09:23:24 -0400

    Created app

---
+++ server.js
@@ -0,0 +1,13 @@
+import  express  from "express"
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+ // setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+res.send("Hello World!");
+});
+
+// setup http server to listen on HTTP_POR
+app.listen(HTTP_PORT);
